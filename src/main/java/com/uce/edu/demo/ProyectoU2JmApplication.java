eb2a89314086d0bbadc98aa9ffde337937a92dbc
David-Masabanda <josue.masabanda99@gmail.com>
2022-06-27 13:28:09 -0500

    Tarea12-log4j

--- src/main/java/com/uce/edu/demo/ProyectoU2JmApplication.java
+++ src/main/java/com/uce/edu/demo/ProyectoU2JmApplication.java
@@ -1,12 +1,15 @@
 package com.uce.edu.demo;
 
+import org.jboss.logging.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProyectoU2JmApplication implements CommandLineRunner{
-
+	
+	private static Logger Log =Logger.getLogger(ProyectoU2JmApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoU2JmApplication.class, args);
 	}
@@ -14,7 +17,7 @@ public class ProyectoU2JmApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("Hola mundo----prueba2");
-		
+		Log.info("Prueba de Log");
 	}
 
 }
